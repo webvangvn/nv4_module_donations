77f25aa4c6e802ab008fff101321f9f506bc6fc8
thucvinh <thucvinh_arc@yahoo.com.vn>
2015-06-24 17:42:47 +0700

    Create search.php

---
+++ modules/donations/search.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @Project NUKEVIET 4.x
+ * @Author Webvang (hoang.nguyen@webvang.vn)
+ * @Copyright (C) 2015 VINADES.,JSC. All rights reserved
+ * @Createdate Tue, 02 Jun 2015 16:35:15 GMT
+ */
+
+if( ! defined( 'NV_IS_MOD_SEARCH' ) ) die( 'Stop!!!' );
+
